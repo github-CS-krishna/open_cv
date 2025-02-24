83014fbebbc93a0b3938b1c11ec670df797599ce
krishna <krishnasubramaiyan2001@gmail.com>
2025-02-24 18:31:41 +0530

    update show FPS and draw triangle

--- python/webcam_recorder_gui/webCamRecorder.py
+++ python/webcam_recorder_gui/webCamRecorder.py
@@ -2,7 +2,10 @@ import cv2
 from tkinter import *
 from PIL import Image,ImageTk
 from datetime import datetime
+from time import time
+from random import randint
 tk = Tk()
+
 #camera properties
 resolution = (800,600)
 cam = cv2.VideoCapture(0)
@@ -10,18 +13,23 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH,resolution[0])
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,resolution[1])
 
 #video
-videowrite = None
-
+videowrite = frame = None
+#fps variables
+previousTime = currentTime = fps = 0
 #control variables
+drawNewRectangle = False
 cameraStatus = True
 recordStatus = False
+#x and y
+x,y = randint(0,680),randint(0,480)
 
 def startRecordVideo():
     global recordStatus,videowrite
-    resolution = (int(cam.get(3)),int(cam.get(4)))
-    videowrite = cv2.VideoWriter(f'./Record/{datetime.now().strftime("%d-%m-%Y %I_%M_%p")}.avi',cv2.VideoWriter.fourcc(*'XVID'),24,resolution)
+    resolution = (int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    videowrite = cv2.VideoWriter(f'./Record/{datetime.now().strftime("%d-%m-%Y %I_%M_%p")}.avi',cv2.VideoWriter.fourcc(*'XVID'),30,resolution)
     recordStatus = True
     stopRecordButton.configure(state="active")
+    
 
 def stopRecordVideo():
     global recordStatus,videowrite
@@ -30,18 +38,33 @@ def stopRecordVideo():
     stopRecordButton.configure(state="disabled")
 
 def closeCamera():
-        global cameraStatus
-        cameraStatus = False
+    global cameraStatus
+    cameraStatus = False
+
+def drawRectangle():
+    global drawNewRectangle
+    drawNewRectangle = True
+    drawRectangleButton.configure(state="disabled")
 
 def openCamera(cameraStat,recordStat):
-    global cam,videowrite,cameraStatus
+    global cam,videowrite,cameraStatus,previousTime
     if(cameraStat): 
         closeCamButton.configure(state="active")
         startRecordButton.configure(state="active")
         
         ret,frame = cam.read()
+        #record video
         if(recordStat):
             videowrite.write(frame)
+        #calculate FPS
+        currentTime = time()
+        fps = 1/(currentTime-previousTime)
+        previousTime = currentTime
+        fps = "FPS " + str(int(fps))
+        cv2.putText(frame,fps,(35,40),cv2.FONT_HERSHEY_SIMPLEX,1,(25,116,207),2,cv2.LINE_AA)
+
+        if(drawNewRectangle):
+            cv2.rectangle(frame,(x,y), (x+120,y+120), (255,0,0), 3)
 
         captured = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         photoImage = Image.fromarray(captured)
@@ -55,6 +78,7 @@ def openCamera(cameraStat,recordStat):
         setCamOffImage()
         closeCamButton.configure(state="disabled")
         startRecordButton.configure(state="disabled")
+        drawRectangleButton.configure(state="disabled")
         cam = cv2.VideoCapture(0)
         cam.set(cv2.CAP_PROP_FRAME_WIDTH,resolution[0])
         cam.set(cv2.CAP_PROP_FRAME_HEIGHT,resolution[1])
@@ -71,18 +95,20 @@ def setCamOffImage():
 imgLabel = Label()
 
 setCamOffImage()
-openCamButton = Button(text="Open Camera", command=lambda:openCamera(cameraStatus,recordStatus),state="active",width=15,height=2)
-closeCamButton = Button(text="Close Camera", command=closeCamera,state="disabled",width=15,height=2)
-startRecordButton = Button(text="Start Record", command=startRecordVideo,state="disabled",width=15,height=2)
-stopRecordButton = Button(text="Stop Record", command=stopRecordVideo,state="disabled",width=15,height=2)
-closeButton = Button(text="Close", command=quitApplication,state="active",width=12,height=2)
+openCamButton = Button(text="Open Camera", command=lambda:openCamera(cameraStatus,recordStatus),state="active",width=12,height=2)
+closeCamButton = Button(text="Close Camera", command=closeCamera,state="disabled",width=12,height=2)
+drawRectangleButton = Button(text="Draw Rectangle", command=drawRectangle,state="active",width=15,height=2)
+startRecordButton = Button(text="Start Record", command=startRecordVideo,state="disabled",width=12,height=2)
+stopRecordButton = Button(text="Stop Record", command=stopRecordVideo,state="disabled",width=12,height=2)
+closeButton = Button(text="Close", command=quitApplication,state="active",width=10,height=2)
 
 #layout
-imgLabel.grid(row=0,column=0,columnspan=5)
+imgLabel.grid(row=0,column=0,columnspan=6)
 openCamButton.grid(row=1,column=0)
 closeCamButton.grid(row=1,column=1)
-startRecordButton.grid(row=1,column=2)
-stopRecordButton.grid(row=1,column=3)
-closeButton.grid(row=1,column=4)
+drawRectangleButton.grid(row=1,column=2)
+startRecordButton.grid(row=1,column=3)
+stopRecordButton.grid(row=1,column=4)
+closeButton.grid(row=1,column=5)
 
 tk.mainloop()
